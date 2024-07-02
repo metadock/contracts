12ae49c0ede89cefaadbced7acfbd2af62b4214b
gabrielstoica <marius_gabriel1998@yahoo.com>
2024-07-02 17:37:13 +0300

    feat(invoice-module): implement 'payInvoice' method and improve docs

--- src/modules/invoice-module/interfaces/IInvoiceModule.sol
+++ src/modules/invoice-module/interfaces/IInvoiceModule.sol
@@ -8,13 +8,34 @@ interface IInvoiceModule {
                                        EVENTS
     //////////////////////////////////////////////////////////////////////////*/
 
-    event InvoiceCreated(uint256 indexed invoiceId, Types.Invoice invoice);
+    event InvoiceCreated(uint256 indexed id, Types.Invoice invoice);
 
     /*//////////////////////////////////////////////////////////////////////////
                                  CONSTANT FUNCTIONS
     //////////////////////////////////////////////////////////////////////////*/
 
+    /// @notice Retrieves the details of the `id` invoice
+    /// @param id The ID of the invoice for which to get the details
+    function getInvoice(uint256 id) external view returns (Types.Invoice memory invoice);
+
     /*//////////////////////////////////////////////////////////////////////////
                                 NON-CONSTANT FUNCTIONS
     //////////////////////////////////////////////////////////////////////////*/
+
+    /// @notice Creates a new invoice
+    ///
+    /// Requirements:
+    /// - `msg.sender` must be a contract implementing the {IContainer} interface
+    ///
+    /// @param invoice The details of the invoice following the {Invoice} struct format
+    /// @return id The on-chain ID of the invoice
+    function createInvoice(Types.Invoice calldata invoice) external returns (uint256 id);
+
+    /// @notice Pays a transfer-based invoice
+    ///
+    /// Notes:
+    /// - `msg.sender` is enforced to be a specific payer address
+    ///
+    /// @param id The ID of the invoice to pay
+    function payInvoice(uint256 id) external payable;
 }
