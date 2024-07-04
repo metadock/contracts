69c3ab4350025552656702ae30fafd8898d3d3e0
gabrielstoica <marius_gabriel1998@yahoo.com>
2024-07-04 15:08:31 +0300

    docs(errors): update documentation and add 'ModuleNotEnabled' error

--- src/libraries/Errors.sol
+++ src/libraries/Errors.sol
@@ -4,11 +4,37 @@ pragma solidity ^0.8.26;
 /// @title Errors
 /// @notice Library containing all custom errors the protocol may revert with
 library Errors {
+    /*//////////////////////////////////////////////////////////////////////////
+                                    CONTAINER
+    //////////////////////////////////////////////////////////////////////////*/
+
+    /// @notice Thrown when `msg.sender` is not the {Container} contract owner
     error Unauthorized();
+
+    /// @notice Thrown when a native token (ETH) withdrawal fails
     error NativeWithdrawFailed();
+
+    /// @notice Thrown when the available native token (ETH) balance is lower than
+    /// the amount requested to be withdrawn
     error InsufficientNativeToWithdraw();
+
+    /// @notice Thrown when the available ERC-20 token balance is lower than
+    /// the amount requested to be withdrawn
     error InsufficientERC20ToWithdraw();
-    error InvalidModule();
+
+    /// @notice Thrown when the deposited ERC-20 token address is zero
     error InvalidAssetZeroAddress();
+
+    /// @notice Thrown when the deposited ERC-20 token amount is zero
     error InvalidAssetZeroAmount();
+
+    /*//////////////////////////////////////////////////////////////////////////
+                                  MODULE-MANAGER
+    //////////////////////////////////////////////////////////////////////////*/
+
+    /// @notice Thrown when the requested module to be enabled is not a contract
+    error InvalidModule();
+
+    /// @notice Thrown when a container tries to execute a method on a non-enabled module
+    error ModuleNotEnabled();
 }
