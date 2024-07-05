c1165c5a90d8fe63016b0495e307b0e07eeef91f
gabrielstoica <marius_gabriel1998@yahoo.com>
2024-07-05 11:10:23 +0300

    test: deploy and label test contracts in base 'setUp' method and deal USDT tokens

--- test/Base.t.sol
+++ test/Base.t.sol
@@ -4,8 +4,9 @@ pragma solidity ^0.8.20;
 import { Events } from "./utils/Events.sol";
 import { Users } from "./utils/Types.sol";
 import { Test } from "forge-std/Test.sol";
-
+import { MockERC20NoReturn } from "./mocks/MockERC20NoReturn.sol";
 import { Container } from "./../src/Container.sol";
+import { InvoiceModule } from "./../src/modules/invoice-module/InvoiceModule.sol";
 
 abstract contract Base_Test is Test, Events {
     /*//////////////////////////////////////////////////////////////////////////
@@ -18,13 +19,24 @@ abstract contract Base_Test is Test, Events {
                                    TEST CONTRACTS
     //////////////////////////////////////////////////////////////////////////*/
 
+    InvoiceModule internal invoiceModule;
     Container internal container;
+    MockERC20NoReturn internal usdt;
 
     /*//////////////////////////////////////////////////////////////////////////
                                   SET-UP FUNCTION
     //////////////////////////////////////////////////////////////////////////*/
 
     function setUp() public virtual {
+        // Deploy test contracts
+        usdt = new MockERC20NoReturn("Tether USD", "USDT", 6);
+        invoiceModule = new InvoiceModule();
+
+        // Label the test contracts so we can easily track them
+        vm.label({ account: address(usdt), newLabel: "USDT" });
+        vm.label({ account: address(invoiceModule), newLabel: "InvoiceModule" });
+
+        // Create test users
         users = Users({ eve: createUser("eve"), bob: createUser("bob") });
     }
 
@@ -45,6 +57,7 @@ abstract contract Base_Test is Test, Events {
     function createUser(string memory name) internal returns (address payable) {
         address payable user = payable(makeAddr(name));
         vm.deal({ account: user, newBalance: 100 ether });
+        deal({ token: address(usdt), to: user, give: 1000000e16 });
 
         return user;
     }
--- test/unit/concrete/container/Container.t.sol
+++ test/unit/concrete/container/Container.t.sol
@@ -5,12 +5,9 @@ import { Base_Test } from "../../../Base.t.sol";
 import { InvoiceModule } from "./../../../../src/modules/invoice-module/InvoiceModule.sol";
 
 contract Container_Unit_Concrete_Test is Base_Test {
-    InvoiceModule invoiceModule;
-
     function setUp() public virtual override {
         Base_Test.setUp();
 
-        invoiceModule = new InvoiceModule();
         address[] memory modules = new address[](1);
         modules[0] = address(invoiceModule);
 
