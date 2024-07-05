06cef6e3eea53cafce338f2e24c2f0a6f304172b
gabrielstoica <marius_gabriel1998@yahoo.com>
2024-07-05 14:46:09 +0300

    test(module-manager): add 'enableModule' unit tests

---
+++ test/unit/concrete/module-manager/enable-module/enableModule.t.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+import { ModuleManager_Unit_Concrete_Test } from "../ModuleManager.t.sol";
+import { InvoiceModule } from "./../../../../../src/modules/invoice-module/InvoiceModule.sol";
+import { Events } from "../../../../utils/Events.sol";
+import { Errors } from "../../../../utils/Errors.sol";
+import { MockModule } from "../../../../mocks/MockModule.sol";
+
+contract EnableModule_Unit_Concrete_Test is ModuleManager_Unit_Concrete_Test {
+    function setUp() public virtual override {
+        ModuleManager_Unit_Concrete_Test.setUp();
+    }
+
+    function test_RevertWhen_InvalidZeroCodeModule() external {
+        // Expect the next call to revert with the {InvalidZeroCodeModule}
+        vm.expectRevert(Errors.InvalidZeroCodeModule.selector);
+
+        // Run the test
+        moduleManager.enableModule({ module: address(0x1) });
+    }
+
+    modifier whenNonZeroCodeModule() {
+        _;
+    }
+
+    function test_EnableModule() external whenNonZeroCodeModule {
+        // Create a new mock module
+        MockModule mockModule = new MockModule();
+
+        // Expect the {ModuleEnabled} to be emitted
+        vm.expectEmit();
+        emit Events.ModuleEnabled({ module: address(mockModule) });
+
+        // Run the test
+        moduleManager.enableModule({ module: address(mockModule) });
+
+        // Assert the module enablement state
+        bool isModuleEnabled = moduleManager.isModuleEnabled({ module: address(mockModule) });
+        assertTrue(isModuleEnabled);
+    }
+}
