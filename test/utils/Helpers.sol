f3a6de80bdc9467c65f76c6890ade140bff9bf3b
gabrielstoica <marius_gabriel1998@yahoo.com>
2024-07-11 12:29:26 +0300

    test: fix create invoice helper

--- test/utils/Helpers.sol
+++ test/utils/Helpers.sol
@@ -9,12 +9,11 @@ library Helpers {
             InvoiceModulesTypes.Invoice({
                 recipient: recipient,
                 status: InvoiceModulesTypes.Status.Pending,
-                frequency: InvoiceModulesTypes.Frequency.Regular,
                 startTime: 0,
                 endTime: uint40(block.timestamp) + 1 weeks,
                 payment: InvoiceModulesTypes.Payment({
                     method: InvoiceModulesTypes.Method.Transfer,
-                    recurrence: InvoiceModulesTypes.Recurrence.OneTime,
+                    recurrence: InvoiceModulesTypes.Recurrence.OneOff,
                     paymentsLeft: 1,
                     asset: address(0),
                     amount: uint128(1 ether)
