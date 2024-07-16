aed988235551983f604f283cf19782af19b05b86
gabrielstoica <marius_gabriel1998@yahoo.com>
2024-07-16 17:50:45 +0300

    test: add generic 'InvoiceModule' modifiers and update errors

--- test/integration/shared/InvoiceModule.t.sol
+++ test/integration/shared/InvoiceModule.t.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.26;
 
 import { Integration_Test } from "../Integration.t.sol";
 import { Types } from "./../../../src/modules/invoice-module/libraries/Types.sol";
-import { Helpers } from "../../utils/Helpers.sol";
 
 abstract contract InvoiceModule_Integration_Shared_Test is Integration_Test {
     Types.Invoice invoice;
@@ -15,10 +14,59 @@ abstract contract InvoiceModule_Integration_Shared_Test is Integration_Test {
         invoice.status = Types.Status.Pending;
     }
 
+    modifier whenCallerContract() {
+        _;
+    }
+
+    modifier whenCompliantContainer() {
+        _;
+    }
+
+    modifier whenNonZeroPaymentAmount() {
+        _;
+    }
+
+    modifier whenStartTimeLowerThanEndTime() {
+        _;
+    }
+
+    modifier whenEndTimeInTheFuture() {
+        _;
+    }
+
+    modifier whenPaymentIntervalLongEnough() {
+        _;
+    }
+
+    modifier whenTranchedStreamWithGoodRecurring() {
+        _;
+    }
+
+    modifier whenPaymentAssetNotNativeToken() {
+        _;
+    }
+
+    modifier givenPaymentMethodOneOffTransfer() {
+        _;
+    }
+
+    modifier givenPaymentMethodRecurringTransfer() {
+        _;
+    }
+
+    modifier givenPaymentMethodTranchedStream() {
+        _;
+    }
+
+    modifier givenPaymentMethodLinearStream() {
+        _;
+    }
+
     /// @dev Creates an invoice with a one-off transfer payment
     function createInvoiceWithOneOffTransfer() internal {
         invoice.startTime = uint40(block.timestamp);
         invoice.endTime = uint40(block.timestamp) + 4 weeks;
+
         invoice.payment = Types.Payment({
             method: Types.Method.Transfer,
             recurrence: Types.Recurrence.OneOff,
@@ -34,13 +82,40 @@ abstract contract InvoiceModule_Integration_Shared_Test is Integration_Test {
         invoice.startTime = uint40(block.timestamp);
         invoice.endTime = uint40(block.timestamp) + 4 weeks;
 
-        uint40 interval = invoice.endTime - invoice.startTime;
-        uint40 numberOfPayments = Helpers.computeNumberOfRecurringPayments(recurrence, interval);
-
         invoice.payment = Types.Payment({
             method: Types.Method.Transfer,
             recurrence: recurrence,
-            paymentsLeft: numberOfPayments,
+            paymentsLeft: 0,
+            asset: address(usdt),
+            amount: 100e18,
+            streamId: 0
+        });
+    }
+
+    /// @dev Creates an invoice with a linear stream-based payment
+    function createInvoiceWithLinearStream() internal {
+        invoice.startTime = uint40(block.timestamp);
+        invoice.endTime = uint40(block.timestamp) + 4 weeks;
+
+        invoice.payment = Types.Payment({
+            method: Types.Method.LinearStream,
+            recurrence: Types.Recurrence.Weekly, // doesn't matter
+            paymentsLeft: 0,
+            asset: address(usdt),
+            amount: 100e18,
+            streamId: 0
+        });
+    }
+
+    /// @dev Creates an invoice with a tranched stream-based payment
+    function createInvoiceWithTranchedStream(Types.Recurrence recurrence) internal {
+        invoice.startTime = uint40(block.timestamp);
+        invoice.endTime = uint40(block.timestamp) + 4 weeks;
+
+        invoice.payment = Types.Payment({
+            method: Types.Method.TranchedStream,
+            recurrence: recurrence,
+            paymentsLeft: 0,
             asset: address(usdt),
             amount: 100e18,
             streamId: 0
