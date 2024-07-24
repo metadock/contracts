f9d2ca446d407e7d36968d7059eb1592107a3c9a
gabrielstoica <marius_gabriel1998@yahoo.com>
2024-07-24 16:58:11 +0300

    docs(src): update license

--- src/modules/invoice-module/libraries/Types.sol
+++ src/modules/invoice-module/libraries/Types.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.26;
 
 /// @notice Namespace for the structs used across the Invoice Module contracts
