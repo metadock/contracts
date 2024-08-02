968ed8d2d701d5d5a305155fdf4b3fb08f492604
gabrielstoica <marius_gabriel1998@yahoo.com>
2024-08-02 14:11:34 +0300

    feat(dock-registry): add basic implementation

---
+++ src/DockRegistry.sol
@@ -0,0 +1,82 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.26;
+
+import { Ownable } from "./abstracts/Ownable.sol";
+import { IDockRegistry } from "./interfaces/IDockRegistry.sol";
+import { Container } from "./Container.sol";
+import { ModuleKeeper } from "./ModuleKeeper.sol";
+import { Errors } from "./libraries/Errors.sol";
+
+/// @title DockRegistry
+/// @notice See the documentation in {IDockRegistry}
+contract DockRegistry is IDockRegistry, Ownable {
+    /*//////////////////////////////////////////////////////////////////////////
+                                   PUBLIC STORAGE
+    //////////////////////////////////////////////////////////////////////////*/
+
+    /// @inheritdoc IDockRegistry
+    ModuleKeeper public immutable override moduleKeeper;
+
+    /*//////////////////////////////////////////////////////////////////////////
+                                   PRIVATE STORAGE
+    //////////////////////////////////////////////////////////////////////////*/
+
+    /// @dev Counter to keep track of the next dock ID
+    uint256 private _dockNextId;
+
+    /// @dev Retrieves the dock ID of the given container address
+    mapping(Container container => uint256 dockId) private _dockIdOfContainer;
+
+    /// @dev Retrieves the owner of the given dock ID
+    mapping(uint256 dockId => address owner) private _ownerOfDock;
+
+    /*//////////////////////////////////////////////////////////////////////////
+                                    CONSTRUCTOR
+    //////////////////////////////////////////////////////////////////////////*/
+
+    /// @dev Intializes the address of the {ModuleKeeper} contract and sets the next dock ID to start from 1
+    constructor(ModuleKeeper _moduleKeeper) Ownable(msg.sender) {
+        _dockNextId = 1;
+        moduleKeeper = _moduleKeeper;
+    }
+
+    /*//////////////////////////////////////////////////////////////////////////
+                                NON-CONSTANT FUNCTIONS
+    //////////////////////////////////////////////////////////////////////////*/
+
+    /// @inheritdoc IDockRegistry
+    function createContainer(
+        uint256 dockId,
+        address owner,
+        address[] memory initialModules
+    ) public returns (Container container) {
+        // Checks: a new dock must be created first
+        if (dockId == 0) {
+            // Store the ID of the next dock
+            dockId = _dockNextId;
+
+            // Effects: set the owner of the freshly created dock
+            _ownerOfDock[dockId] = msg.sender;
+
+            // Effects: increment the next dock ID
+            // Use unchecked because the dock ID cannot realistically overflow
+            unchecked {
+                _dockNextId++;
+            }
+        } else {
+            // Checks: `msg.sender` is the dock owner
+            if (_ownerOfDock[dockId] != msg.sender) {
+                revert Errors.SenderNotDockOwner();
+            }
+        }
+
+        // Interactions: deploy a new {Container}
+        container = new Container({ _owner: owner, _moduleKeeper: moduleKeeper, _initialModules: initialModules });
+
+        // Assign the ID of the dock to which the new container belongs
+        _dockIdOfContainer[container] = _dockNextId;
+
+        // Log the {Container} creation
+        emit ContainerCreated(owner, dockId, container, initialModules);
+    }
+}
---
+++ src/interfaces/IDockRegistry.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.26;
+
+import { Container } from "./../Container.sol";
+import { ModuleKeeper } from "./../ModuleKeeper.sol";
+
+/// @title IDockRegistry
+/// @notice Contract that provides functionalities to create docks and deploy {Container}s from a single place
+interface IDockRegistry {
+    /*//////////////////////////////////////////////////////////////////////////
+                                       EVENTS
+    //////////////////////////////////////////////////////////////////////////*/
+
+    /// @notice Emitted when a new {Container} contract gets deployed
+    /// @param owner The address of the owner
+    /// @param dockId The ID of the dock to which this {Container} belongs
+    /// @param container The address of the {Container}
+    /// @param initialModules Array of initially enabled modules
+    event ContainerCreated(
+        address indexed owner, uint256 indexed dockId, Container container, address[] initialModules
+    );
+
+    /*//////////////////////////////////////////////////////////////////////////
+                                 CONSTANT FUNCTIONS
+    //////////////////////////////////////////////////////////////////////////*/
+
+    /// @notice Returns the address of the {ModuleKeeper} contract
+    function moduleKeeper() external view returns (ModuleKeeper);
+
+    /*//////////////////////////////////////////////////////////////////////////
+                                NON-CONSTANT FUNCTIONS
+    //////////////////////////////////////////////////////////////////////////*/
+
+    /// @notice Creates a new {Container} contract and attaches it to a dock
+    ///
+    /// Notes:
+    /// - if `dockId` equal zero, a new dock will be created
+    ///
+    /// Requirements:
+    /// - `msg.sender` MUST be the dock owner
+    ///
+    /// @param dockId The ID of the dock to attach the {Container} to
+    /// @param owner The address of the {Container} owner
+    /// @param initialModules Array of initially enabled modules
+    function createContainer(
+        uint256 dockId,
+        address owner,
+        address[] memory initialModules
+    ) external returns (Container container);
+}
