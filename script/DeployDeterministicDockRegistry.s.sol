bccb1e02ec4fb43d36c45e1045ad5b96289db7a9
gabrielstoica <marius_gabriel1998@yahoo.com>
2024-08-08 17:25:26 +0300

    chore: update 'DockRegistry' deployment script to use 'openzeppelin-foundry-upgrades'

--- script/DeployDeterministicDockRegistry.s.sol
+++ script/DeployDeterministicDockRegistry.s.sol
@@ -1,6 +1,10 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.26;
 
+import { ERC1967Proxy } from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+import { Options } from "./../lib/openzeppelin-foundry-upgrades/src/Options.sol";
+import { Core } from "./../lib/openzeppelin-foundry-upgrades/src/internal/Core.sol";
+
 import { BaseScript } from "./Base.s.sol";
 import { DockRegistry } from "./../src/DockRegistry.sol";
 import { ModuleKeeper } from "./../src/ModuleKeeper.sol";
@@ -18,6 +22,25 @@ contract DeployDeterministicDockRegistry is BaseScript {
         bytes32 salt = bytes32(abi.encodePacked(create2Salt));
 
         // Deterministically deploy a {DockRegistry} contract
-        dockRegistry = new DockRegistry{ salt: salt }(initialOwner, moduleKeeper);
+        dockRegistry = DockRegistry(
+            deployDetermisticUUPSProxy(
+                salt, "DockRegistry.sol", abi.encodeCall(DockRegistry.initialize, (initialOwner, moduleKeeper))
+            )
+        );
+    }
+
+    /// @dev Deploys a UUPS proxy at deterministic addresses across chains based on a provided salt
+    /// @param salt Salt to use for deterministic deployment
+    /// @param contractName The name of the implementation contract
+    /// @param initializerData The ABI encoded call to be made to the initialize method
+    function deployDetermisticUUPSProxy(
+        bytes32 salt,
+        string memory contractName,
+        bytes memory initializerData
+    ) internal returns (address) {
+        Options memory opts;
+        address impl = Core.deployImplementation(contractName, opts);
+
+        return address(new ERC1967Proxy{ salt: salt }(impl, initializerData));
     }
 }
