ed9f0889c8f85d72f752ad37f9abcdb12500b1ee
gabrielstoica <marius_gabriel1998@yahoo.com>
2024-08-09 17:24:20 +0300

    test: rename 'OwnableMock' to 'MockOwnable'

--- test/mocks/MockOwnable.sol
+++ test/mocks/MockOwnable.sol
@@ -3,8 +3,8 @@ pragma solidity ^0.8.26;
 
 import { Ownable } from "./../../src/abstracts/Ownable.sol";
 
-/// @title OwnableMock
+/// @title MockOwnable
 /// @notice A mock implementation that uses the `onlyOwner` auth mechanism
-contract OwnableMock is Ownable {
+contract MockOwnable is Ownable {
     constructor(address _owner) Ownable(_owner) { }
 }
--- test/unit/shared/Ownable.t.sol
+++ test/unit/shared/Ownable.t.sol
@@ -2,14 +2,14 @@
 pragma solidity ^0.8.26;
 
 import { Base_Test } from "../../Base.t.sol";
-import { OwnableMock } from "../../mocks/OwnableMock.sol";
+import { MockOwnable } from "../../mocks/MockOwnable.sol";
 
 contract Ownable_Shared_Test is Base_Test {
-    OwnableMock ownableMock;
+    MockOwnable ownableMock;
 
     function setUp() public virtual override {
         Base_Test.setUp();
-        ownableMock = new OwnableMock({ _owner: users.admin });
+        ownableMock = new MockOwnable({ _owner: users.admin });
     }
 
     modifier whenCallerCurrentOwner() {
