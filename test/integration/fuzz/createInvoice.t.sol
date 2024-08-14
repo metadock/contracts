2842874c05cc811d571a8bb9b660fb6d313434d7
gabrielstoica <marius_gabriel1998@yahoo.com>
2024-08-14 13:59:24 +0300

    test: merge 'createInvoice' fuzz tests and add 'payInvoice' fuzz tests

--- test/integration/fuzz/createInvoice.t.sol
+++ test/integration/fuzz/createInvoice.t.sol
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.26;
 
-import { CreateInvoice_Integration_Shared_Test } from "../../shared/createInvoice.t.sol";
-import { Types } from "./../../../../src/modules/invoice-module/libraries/Types.sol";
-import { Helpers } from "./../../../../src/modules/invoice-module/libraries/Helpers.sol";
-import { Events } from "../../../utils/Events.sol";
+import { CreateInvoice_Integration_Shared_Test } from "../shared/createInvoice.t.sol";
+import { Types } from "./../../../src/modules/invoice-module/libraries/Types.sol";
+import { Helpers } from "../../utils/Helpers.sol";
+import { Events } from "../../utils/Events.sol";
 
-contract CreateInvoice_Transfer_Integration_Fuzz_Test is CreateInvoice_Integration_Shared_Test {
+contract CreateInvoice_Integration_Fuzz_Test is CreateInvoice_Integration_Shared_Test {
     Types.Invoice invoice;
 
     function setUp() public virtual override {
@@ -16,8 +16,9 @@ contract CreateInvoice_Transfer_Integration_Fuzz_Test is CreateInvoice_Integrati
         vm.startPrank({ msgSender: users.eve });
     }
 
-    function testFuzz_CreateInvoice_Transfer(
+    function testFuzz_CreateInvoice(
         uint8 recurrence,
+        uint8 paymentMethod,
         address recipient,
         uint40 startTime,
         uint40 endTime,
@@ -29,26 +30,39 @@ contract CreateInvoice_Transfer_Integration_Fuzz_Test is CreateInvoice_Integrati
         whenNonZeroPaymentAmount
         whenStartTimeLowerThanEndTime
         whenEndTimeInTheFuture
-        givenPaymentMethodLinearStream
         whenPaymentAssetNotNativeToken
     {
-        // Discard the bad fuzz inputs
-        recurrence = uint8(bound(recurrence, uint8(Types.Recurrence.OneOff), uint8(Types.Recurrence.Yearly)));
+        // Discard bad fuzz inputs
+        // Assume recurrence is within Types.Recurrence enum values (OneOff, Weekly, Monthly, Yearly) (0, 1, 2, 3)
+        vm.assume(recurrence < 4);
+        // Assume recurrence is within Types.Method enum values (Transfer, LinearStream, TranchedStream) (0, 1, 2)
+        vm.assume(paymentMethod < 3);
         vm.assume(recipient != address(0) && recipient != address(this));
         vm.assume(startTime >= uint40(block.timestamp) && startTime < endTime);
         vm.assume(amount > 0);
 
-        // Check if the interval is too short for the fuzzed recurrence
-        uint40 numberOfPayments =
-            Helpers.computeNumberOfPayments({ recurrence: Types.Recurrence(recurrence), interval: endTime - startTime });
-        if (numberOfPayments == 0) return;
+        // Calculate the number of payments if this is a transfer-based invoice
+        (bool valid, uint40 numberOfPayments) =
+            Helpers.checkFuzzedPaymentMethod(paymentMethod, recurrence, startTime, endTime);
+        if (!valid) return;
 
-        // Create a new invoice with a linear stream payment
-        invoice = createFuzzedInvoice(
-            Types.Method.Transfer, Types.Recurrence(recurrence), recipient, startTime, endTime, amount
-        );
+        // Create a new invoice with a transfer-based payment
+        invoice = Types.Invoice({
+            recipient: recipient,
+            status: Types.Status.Pending,
+            startTime: startTime,
+            endTime: endTime,
+            payment: Types.Payment({
+                recurrence: Types.Recurrence(recurrence),
+                method: Types.Method(paymentMethod),
+                paymentsLeft: numberOfPayments,
+                amount: amount,
+                asset: address(usdt),
+                streamId: 0
+            })
+        });
 
-        // Create the calldata for the Invoice Module execution
+        // Create the calldata for the {InvoiceModule} execution
         bytes memory data = abi.encodeWithSignature(
             "createInvoice((address,uint8,uint40,uint40,(uint8,uint8,uint40,address,uint128,uint256)))", invoice
         );
@@ -56,8 +70,8 @@ contract CreateInvoice_Transfer_Integration_Fuzz_Test is CreateInvoice_Integrati
         // Expect the module call to emit an {InvoiceCreated} event
         vm.expectEmit();
         emit Events.InvoiceCreated({
-            id: 0,
-            recipient: recipient,
+            id: 1,
+            recipient: invoice.recipient,
             status: Types.Status.Pending,
             startTime: invoice.startTime,
             endTime: invoice.endTime,
@@ -72,8 +86,8 @@ contract CreateInvoice_Transfer_Integration_Fuzz_Test is CreateInvoice_Integrati
         container.execute({ module: address(invoiceModule), value: 0, data: data });
 
         // Assert the actual and expected invoice state
-        Types.Invoice memory actualInvoice = invoiceModule.getInvoice({ id: 0 });
-        assertEq(actualInvoice.recipient, recipient);
+        Types.Invoice memory actualInvoice = invoiceModule.getInvoice({ id: 1 });
+        assertEq(actualInvoice.recipient, invoice.recipient);
         assertEq(uint8(actualInvoice.status), uint8(Types.Status.Pending));
         assertEq(actualInvoice.startTime, invoice.startTime);
         assertEq(actualInvoice.endTime, invoice.endTime);
@@ -82,5 +96,6 @@ contract CreateInvoice_Transfer_Integration_Fuzz_Test is CreateInvoice_Integrati
         assertEq(actualInvoice.payment.asset, invoice.payment.asset);
         assertEq(actualInvoice.payment.amount, invoice.payment.amount);
         assertEq(actualInvoice.payment.streamId, 0);
+        assertEq(actualInvoice.payment.paymentsLeft, invoice.payment.paymentsLeft);
     }
 }
---
+++ test/integration/fuzz/payInvoice.t.sol
@@ -0,0 +1,124 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.26;
+
+import { PayInvoice_Integration_Shared_Test } from "../shared/payInvoice.t.sol";
+import { Types } from "./../../../src/modules/invoice-module/libraries/Types.sol";
+import { Events } from "../../utils/Events.sol";
+import { Helpers } from "../../utils/Helpers.sol";
+
+contract PayInvoice_Integration_Fuzz_Test is PayInvoice_Integration_Shared_Test {
+    Types.Invoice invoice;
+
+    function setUp() public virtual override {
+        PayInvoice_Integration_Shared_Test.setUp();
+    }
+
+    function testFuzz_PayInvoice(
+        uint8 recurrence,
+        uint8 paymentMethod,
+        uint40 startTime,
+        uint40 endTime,
+        uint128 amount
+    )
+        external
+        whenInvoiceNotNull
+        whenInvoiceNotAlreadyPaid
+        whenInvoiceNotCanceled
+        givenPaymentMethodTransfer
+        givenPaymentAmountInNativeToken
+        whenPaymentAmountEqualToInvoiceValue
+        whenNativeTokenPaymentSucceeds
+    {
+        // Discard bad fuzz inputs
+        // Assume recurrence is within Types.Recurrence enum values (OneOff, Weekly, Monthly, Yearly) (0, 1, 2, 3)
+        vm.assume(recurrence < 4);
+        // Assume recurrence is within Types.Method enum values (Transfer, LinearStream, TranchedStream) (0, 1, 2)
+        vm.assume(paymentMethod < 3);
+        vm.assume(startTime >= uint40(block.timestamp) && startTime < endTime);
+        vm.assume(amount > 0);
+
+        // Calculate the number of payments if this is a transfer-based invoice
+        (bool valid, uint40 numberOfPayments) =
+            Helpers.checkFuzzedPaymentMethod(paymentMethod, recurrence, startTime, endTime);
+        if (!valid) return;
+
+        // Create a new invoice with a transfer-based payment
+        invoice = Types.Invoice({
+            recipient: users.eve,
+            status: Types.Status.Pending,
+            startTime: startTime,
+            endTime: endTime,
+            payment: Types.Payment({
+                recurrence: Types.Recurrence(recurrence),
+                method: Types.Method(paymentMethod),
+                paymentsLeft: numberOfPayments,
+                amount: amount,
+                asset: address(usdt),
+                streamId: 0
+            })
+        });
+
+        // Create the calldata for the {InvoiceModule} execution
+        bytes memory data = abi.encodeWithSignature(
+            "createInvoice((address,uint8,uint40,uint40,(uint8,uint8,uint40,address,uint128,uint256)))", invoice
+        );
+
+        // Make Eve the caller to create the fuzzed invoice
+        vm.startPrank({ msgSender: users.eve });
+
+        // Create the fuzzed invoice
+        container.execute({ module: address(invoiceModule), value: 0, data: data });
+
+        // Mint enough USDT to the payer's address to be able to pay the invoice
+        deal({ token: address(usdt), to: users.bob, give: invoice.payment.amount });
+
+        // Make payer the caller to pay for the fuzzed invoice
+        vm.startPrank({ msgSender: users.bob });
+
+        // Approve the {InvoiceModule} to transfer the USDT tokens on payer's behalf
+        usdt.approve({ spender: address(invoiceModule), amount: invoice.payment.amount });
+
+        // Store the USDT balances of the payer and recipient before paying the invoice
+        uint256 balanceOfPayerBefore = usdt.balanceOf(users.bob);
+        uint256 balanceOfRecipientBefore = usdt.balanceOf(users.eve);
+
+        uint256 streamId = paymentMethod == 0 ? 0 : 1;
+        numberOfPayments = numberOfPayments > 0 ? numberOfPayments - 1 : 0;
+
+        Types.Status expectedInvoiceStatus = numberOfPayments == 0 && invoice.payment.method == Types.Method.Transfer
+            ? Types.Status.Paid
+            : Types.Status.Ongoing;
+
+        // Expect the {InvoicePaid} event to be emitted
+        vm.expectEmit();
+        emit Events.InvoicePaid({
+            id: 1,
+            payer: users.bob,
+            status: expectedInvoiceStatus,
+            payment: Types.Payment({
+                method: invoice.payment.method,
+                recurrence: invoice.payment.recurrence,
+                paymentsLeft: numberOfPayments,
+                asset: invoice.payment.asset,
+                amount: invoice.payment.amount,
+                streamId: streamId
+            })
+        });
+
+        // Run the test
+        invoiceModule.payInvoice({ id: 1 });
+
+        // Assert the actual and the expected state of the invoice
+        Types.Invoice memory actualInvoice = invoiceModule.getInvoice({ id: 1 });
+        assertEq(uint8(actualInvoice.status), uint8(expectedInvoiceStatus));
+        assertEq(actualInvoice.payment.paymentsLeft, numberOfPayments);
+
+        // Assert the actual and expected balances of the payer and recipient
+        assertEq(usdt.balanceOf(users.bob), balanceOfPayerBefore - invoice.payment.amount);
+        if (invoice.payment.method == Types.Method.Transfer) {
+            assertEq(usdt.balanceOf(users.eve), balanceOfRecipientBefore + invoice.payment.amount);
+        } else {
+            assertEq(usdt.balanceOf(users.eve), balanceOfRecipientBefore);
+        }
+    }
+}
