6e0b926ac5132707c7aee4151d891b48a9a52ab1
gabrielstoica <marius_gabriel1998@yahoo.com>
2024-10-04 13:49:21 +0300

    test: add integration concrete tests for 'InvoiceModule.transferFrom' method

---
+++ test/integration/shared/transferFrom.t.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.26;
+
+import { Integration_Test } from "../Integration.t.sol";
+import { PayInvoice_Integration_Shared_Test } from "./payInvoice.t.sol";
+
+abstract contract TransferFrom_Integration_Shared_Test is Integration_Test, PayInvoice_Integration_Shared_Test {
+    function setUp() public virtual override(Integration_Test, PayInvoice_Integration_Shared_Test) {
+        PayInvoice_Integration_Shared_Test.setUp();
+    }
+
+    modifier whenTokenExists() {
+        _;
+    }
+}
